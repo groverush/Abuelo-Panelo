71917f5c8fee95a4d7f0ea32df230a11c49e8e50
sebascg17 <sebas-clavi@hotmail.com>
2025-06-28 03:10:17 -0500

    Se desarrolla la mecanica de cortar junto con la animacion y la recoleccion de la caña

---
+++ Assets/Scripts/SugarcanePiece.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class SugarcanePiece : MonoBehaviour
+{
+    public float velocidadFlotacion = 1f;
+    public float amplitud = 0.25f;
+    public float velocidadRotacion = 60f;
+
+    private Vector3 posicionInicial;
+
+    private void Start ()
+    {
+        posicionInicial = transform.position;
+    }
+
+    private void Update ()
+    {
+        // Movimiento de flotación
+        float nuevaY = posicionInicial.y + Mathf.Sin(Time.time * velocidadFlotacion) * amplitud;
+        transform.position = new Vector3(posicionInicial.x, nuevaY, posicionInicial.z);
+
+        // Rotación constante
+        transform.Rotate(Vector3.up * velocidadRotacion * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerEnter ( Collider other )
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerController jugador = other.GetComponent<PlayerController>();
+            if (jugador != null && jugador.PuedeRecolectarCana())
+            {
+                jugador.RecolectarCana();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
